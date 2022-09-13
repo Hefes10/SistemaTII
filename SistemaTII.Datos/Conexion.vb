64741d7f439fae53ab50c17a99bb95419b19aa57
Martin Miret <jmiret.crowdar@supervielle.com.ar>
2022-09-12 21:14:22 -0300

    Conexion a DB y Entidad Categoria

---
+++ SistemaTII.Datos/Conexion.vb
@@ -0,0 +1,8 @@
+﻿Imports System.Data.SqlClient
+Public Class Conexion
+    Public conn As SqlConnection
+
+    Public Sub New()
+        Me.conn = New SqlConnection("Data Source=CROWDAR-IDEAPAD\MSSQLSERVER03;Initial Catalog=dbsistema;Integrated Security=True")
+    End Sub
+End Class
